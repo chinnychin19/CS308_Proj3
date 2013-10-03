2d804bc09941b704e523bfb8cbafbc541737dda2
Lalita Maraj <lalita.maraj@duke.edu>
2013-10-02 23:53:27 -0500

    10/2_Implemnted View Class with execution commands

--- src/view/View.java
+++ src/view/View.java
@@ -1,5 +1,31 @@
 package view;
 
-public class View {
+import model.Model;
+import view.display.ViewUpdater;
+import view.input.RunButton;
+import view.input.Textbox;
+import view.sidebar.SideBar;
 
+
+public class View {
+    protected ViewUpdater myViewUpdater;
+    protected RunButton myRunButton;
+    protected Textbox myTextbox;
+    protected SideBar mySidebar;
+    protected Model myModel;
+
+    public View () {
+
+    }
+    protected void executeInput(){
+        //when runButton is activated
+        //input is sent to model
+        String input = "";
+//        myModel.myInterpreter.parseInput(input);     
+        //Clear textbox    
+        myViewUpdater.displayOutput();
+        
+        mySidebar.update();
+    }
+    
 }
--- src/view/display/ViewUpdater.java
+++ src/view/display/ViewUpdater.java
@@ -1,7 +1,7 @@
 package view.display;
 
 public class ViewUpdater {
-
+    
     public ViewUpdater () {
         // TODO Auto-generated constructor stub
     }
