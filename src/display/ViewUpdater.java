b8425d4df9576ae6ddab4ee41ebe7c0eab43bd25
Susan Zhang <sz56@duke.edu>
2013-10-02 21:53:33 -0500

    10/2_Adding front end skeleton classes

---
+++ src/display/ViewUpdater.java
@@ -0,0 +1,16 @@
+package display;
+
+public class ViewUpdater {
+
+    public ViewUpdater () {
+        // TODO Auto-generated constructor stub
+    }
+    
+    public void displayOutput(){
+//        processNext();
+    }
+   
+
+    
+    
+}
