809576b910c6dfba347c5cea4ac92b1fd6a0844c
Lalita Maraj <lalita.maraj@duke.edu>
2013-10-06 17:36:51 -0400

    10/6_started separate JPanel classes

--- src/view/Display.java
+++ src/view/Display.java
@@ -1,6 +1,7 @@
 package view;
 
 import java.awt.Dimension;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 
@@ -9,5 +10,8 @@ public class Display extends JPanel {
     public Display (int width, int height) {
         super();
         this.setPreferredSize(new Dimension(width, height));
+
+        // prompt1 = new JLabel("I have a number between 1 and 1000.");
+        this.add(new JLabel("I have a number between 1 and 1000."));
     }
 }
