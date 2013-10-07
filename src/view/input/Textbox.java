4cdfe0784f8cd7918ac0457dbd3d2d62a9473629
Lalita Maraj <lalita.maraj@duke.edu>
2013-10-07 14:31:48 -0400

    10/7_Extracted Input Panel

--- src/view/input/Textbox.java
+++ src/view/input/Textbox.java
@@ -1,34 +1,27 @@
 package view.input;
 
-
 import javax.swing.JTextField;
 
-public class Textbox extends JTextField {
-    
 
-    public Textbox(int fieldSize){
+public class Textbox extends JTextField {
+
+    public Textbox (int fieldSize) {
         super(fieldSize);
 
     }
+
     public void clear () {
-       this.setText("");
+        this.setText("");
     }
 
     public String getInput () {
 
-        return this.getText();
+        return this.getText().trim();
     }
 
     public void addInput (String input) {
-        String newInput = input;
-        if (!getInput().equals("")){
-            newInput = getInput();
-            newInput += " ";
-            newInput += input;
-            
-        }
-      
-        this.setText(newInput );
+        String newInput = getInput() + input;
+        this.setText(newInput);
     }
 
 }
