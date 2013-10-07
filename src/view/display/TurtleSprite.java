3e832a578cb826258179839f379482942366da09
Susan Zhang <sz56@duke.edu>
2013-10-07 13:17:00 -0400

    10/7_Checking for merge conflict

--- src/view/display/TurtleSprite.java
+++ src/view/display/TurtleSprite.java
@@ -1,11 +1,12 @@
 package view.display;
 
 import jgame.JGObject;
+import jgame.platform.JGEngine;
 
 public class TurtleSprite extends JGObject {
 
     public TurtleSprite (JGEngine engine, double x, double y, int CID) {
-      
+      super("turtle", true, x, y, CID, "turtlePic", 0, 0, 0, 0,-1);
     }
 
 }
