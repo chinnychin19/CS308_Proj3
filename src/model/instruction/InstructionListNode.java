350e4bf126b95f0a5300be4a73a720840102f696
Ken McAndrews <ken.mcandrews@gmail.com>
2013-10-08 18:05:07 -0400

    10/8_Added InstructionListNode class

---
+++ src/model/instruction/InstructionListNode.java
@@ -0,0 +1,15 @@
+package model.instruction;
+
+public class InstructionListNode extends Instruction {
+
+    public InstructionListNode (Instruction parent) {
+        super(-1, parent);
+    }
+
+    @Override
+    public Instruction eval () {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
