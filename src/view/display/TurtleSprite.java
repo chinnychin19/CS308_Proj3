4fc56ab1b2fb7b940a90f5f3816f69d2e1e00f22
Susan Zhang <sz56@duke.edu>
2013-10-09 01:12:23 -0400

    10/8_Added background color changing, return to origin

--- src/view/display/TurtleSprite.java
+++ src/view/display/TurtleSprite.java
@@ -3,23 +3,31 @@ package view.display;
 import jgame.JGObject;
 import jgame.platform.JGEngine;
 
-
+/**
+ * Class that 
+ * @author susanzhang93
+ *
+ */
 public class TurtleSprite extends JGObject {
-
+    
+    /**
+     * Constructor for TurtleSprite class
+     * 
+     * @param engine JGEngine in which TurtleSprite JGObject is created
+     * @param x initial x position of TurtleSprite
+     * @param y initial y position of TurtleSprite
+     * @param CID Collision ID of TurtleSprite
+     */
     public TurtleSprite (JGEngine engine, double x, double y, int CID) {
       super("turtle", true, x, y, CID, "turtleGif", 0, 0, 0, 0,-1);
     }
 
-    /**
-     * Moves the turtle, draws line between old and new turtle location
-     * 
-     * @param x new x location of turtle
-     * @param y new y location of turtle
-     */
-    protected void moveSprite(double x, double y){
-        this.setPos(x, y);
-        
-    }
+    //Temporarily here for testing
+    public TurtleSprite (double x, double y, int CID) {
+        super("turtle", true, x, y, CID, "turtleGif", 0, 0, 0, 0,-1);
+      }
+
+  
     
     
 }
