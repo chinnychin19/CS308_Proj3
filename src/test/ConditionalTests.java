eb0e5ba917147fad5b9ccf749cc040dc5a5cb128
Ken McAndrews <ken.mcandrews@gmail.com>
2013-10-10 14:56:12 -0400

    10/10_Added initial conditional tests

--- src/test/ConditionalTests.java
+++ src/test/ConditionalTests.java
@@ -7,11 +7,38 @@ import org.junit.Test;
 
 public class ConditionalTests {
 
+    private static final double DELTA = 0.0001;
+
     @Test
     public void testIF () {
-        // TODO: Finish tests
         Model.initModel();
-        Model.parseInput("if ");
+        Model.parseInput("if 1 [ fd 10 ]");
+        Model.parseInput("if 0 [ fd 10 ]");
+        Model.parseInput("if sum 1 2 [ fd 10 ]");
+        Model.parseInput("fd if 1 [ if 1 [ fd 10 ] ]");
+        Model.processNextInstruction();
+        assertEquals(10, Model.getTurtleY(), DELTA);
+        Model.processNextInstruction();
+        assertEquals(10, Model.getTurtleY(), DELTA);
+        Model.processNextInstruction();
+        assertEquals(20, Model.getTurtleY(), DELTA);
+        Model.processNextInstruction();
+        assertEquals(40, Model.getTurtleY(), DELTA);
+    }
+
+    @Test
+    public void testIFELSE () {
+        // TODO: Finish test
+        Model.initModel();
+        Model.parseInput("if 1 [ fd 10 ]");
+        Model.parseInput("if 0 [ fd 10 ]");
+        Model.parseInput("if sum 1 2 [ fd 10 ]");
+        Model.processNextInstruction();
+        assertEquals(10, Model.getTurtleY(), DELTA);
+        Model.processNextInstruction();
+        assertEquals(10, Model.getTurtleY(), DELTA);
+        Model.processNextInstruction();
+        assertEquals(20, Model.getTurtleY(), DELTA);
     }
 
 }
