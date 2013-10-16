fa3898e23b1531dd38ff3abe0b0a5ebc56308889
Lalita Maraj <lalita.maraj@duke.edu>
2013-10-16 18:54:55 -0400

    10/16_Integrated Options Panel in Factory

---
+++ src/view/optionsPanel/OptionsPanel.java
@@ -0,0 +1,25 @@
+package view.optionsPanel;
+
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+
+public class OptionsPanel extends JPanel {
+
+    public OptionsPanel (JButton penColorChooser, JButton bgColorChooser,
+                         JCheckBox statusCheckBox, JCheckBox gridCheckBox,
+                         JComboBox<?> imageChooserButton, JButton helpMeButton) {
+        super();
+        this.setLayout(new GridLayout(1, 0));
+        this.add(statusCheckBox);
+        this.add(gridCheckBox);
+        this.add(penColorChooser);
+        this.add(bgColorChooser);
+        this.add(imageChooserButton);
+        this.add(helpMeButton);
+
+    }
+
+}
