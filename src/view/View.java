7760a1a13205591d5c5cf36a99a7f26608c93ca8
Lalita Maraj <lalita.maraj@duke.edu>
2013-10-18 19:49:19 -0400

    10/18_Removed canvas and unused code in View

--- src/view/View.java
+++ src/view/View.java
@@ -2,56 +2,31 @@ package view;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Collection;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
-import javax.swing.AbstractAction;
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JColorChooser;
-import javax.swing.JComboBox;
 import javax.swing.JComponent;
-import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
 import javax.swing.JPanel;
-import jgame.JGColor;
 import menuBar.MenuBar;
 import model.Model;
-import model.Path;
 import view.display.Canvas;
 import view.display.ViewUpdater;
 import view.inputPanel.InputController;
 import view.inputPanel.RunButton;
 import view.inputPanel.Textbox;
-import view.modulePanel.ModuleData;
-import view.modulePanel.ModulePanel;
 import view.modulePanel.ModulePanelController;
 import view.optionsPanel.BackgroundColorChooser;
 import view.optionsPanel.GridCheckBox;
 import view.optionsPanel.ImageChooser;
-import view.optionsPanel.OptionsPanel;
 import view.optionsPanel.PenColorChooser;
 import view.optionsPanel.StatusCheckBox;
 
 
-public class View extends JFrame implements Observer {
+public class View extends JFrame {
     protected ViewUpdater myViewUpdater;
 
     private static Canvas myCanvas;
     private Model myModel;
-    private ControllerDraft myController;
     JPanel modulePanel;
     Textbox textbox;
     RunButton runbutton;
@@ -64,7 +39,7 @@ public class View extends JFrame implements Observer {
     public View (Model model) {
         myModel = model;
         textbox = new Textbox(Constants.FIELD_SIZE);
-        myController = new ControllerDraft(myModel, this, textbox);
+
         setTitle("SLogo");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setMinimumSize(new Dimension(Constants.GUI_WIDTH, Constants.GUI_HEIGHT));
@@ -99,80 +74,14 @@ public class View extends JFrame implements Observer {
 
         setVisible(true);
 
-        // Model.initModel();
-
     }
-
-    public void runCommand () {
-        myController.executeCommand();
-    }
-
-    // SUSAN BEGIN COMPLETING METHODS
-
-    protected void changeWorkSpace () {
-        // TODO
-        // CLEAR Textbox and everything else
-    }
-
-    protected void updateBGColor () {
-        // TODO
-
-    }
-
-    protected void updateModulePanel (Map<String, Collection<ModuleData>> map) {
-        ((ModulePanel) modulePanel).updateModules(map);
-    }
-
-    protected void updateCanvasPanel () {
-        // SUSAN FIX THE INDEX VALUES PLEASE :)
-
-        ArrayList<Integer> activeTurtleList = new ArrayList<Integer>(); // myModel.getActiveTurtleIDs
-                                                                        // ();
-        activeTurtleList.add(1);
-        myCanvas.setActiveTurtles(activeTurtleList);
-
-        // System.out.println(activeTurtleList);
-
-        for (Integer ID : activeTurtleList) {
-            myCanvas.moveTurtle(ID, myModel.getTurtleX(ID), myModel.getTurtleY(ID));
-            myCanvas.setHeading(ID, myModel.getTurtleAngle(ID));
-            myCanvas.changeTurtleVisiblity(myModel.isTurtleVisible(1));
-        }
-
-        myCanvas.setPaths(myModel.getTurtlePaths());
-
-    }
-
-    protected void updateOptionsPanel () {
-        // TODO
-    }
-
-    // SUSAN END COMPLETING METHODS :)
 
     protected void changeModel (Model newModel) {
         myModel = newModel;
     }
 
-    public void displayError (String error) {
-        myCanvas.setError(error);
-    }
-
     public Canvas getCanvas () {
         return myCanvas;
-    }
-
-    @Override
-    public void update (String error,
-                        String updateVariable,
-                        Map<String, Collection<ModuleData>> moduleMap,
-                        ArrayList<Integer> activeTurtleList,
-                        Map<Integer, Double> turtleXMap,
-                        Map<Integer, Double> turtleYMap,
-                        Map<Integer, Double> turtleAngleMap,
-                        Map<Integer, Boolean> turtleVisibilityMap,
-                        Collection<Path> paths) {
-        displayError(error);
-
     }
 
 }
