1ec6724fffba632bacb56c877669c8b00c6b5cd7
Lalita Maraj <lalita.maraj@duke.edu>
2013-10-21 14:16:06 -0400

    Revert 34bbc22..906e1d8
    This rolls back to commit 34bbc22168c5f12921ea0d88b41e2cd52f21de31.

--- src/view/inputPanel/InputObserver.java
+++ src/view/inputPanel/InputObserver.java
@@ -2,10 +2,6 @@ package view.inputPanel;
 
 public interface InputObserver {
 
-    /**Passes booleans to notify whether actions can be performed
-     * @param canUndo indicates whether undo is currently doable
-     * @param canRedo indicates whether redo is currently doable
-     */
     void update (Boolean canUndo, Boolean canRedo);
 
 }
