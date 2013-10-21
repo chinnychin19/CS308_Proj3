20f8f2ae726d4a39ad329c30478ad5dd5083b1d2
Susan Zhang <sz56@duke.edu>
2013-10-20 22:05:53 -0400

    10/20_Parser able to read preference XML file

--- src/view/workspace/Parser.java
+++ src/view/workspace/Parser.java
@@ -2,20 +2,27 @@ package view.workspace;
 
 import java.io.File;
 import java.io.IOException;
+import javax.swing.JFileChooser;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+import org.w3c.dom.Node;
+
 
 public class Parser {
 
+    private static final JFileChooser INPUT_CHOOSER =
+            new JFileChooser(System.getProperties().getProperty("user.dir"));
+    
     public Parser () {
         // TODO Auto-generated constructor stub
     }
-    
-    public void loadPreferences(File prefFile){
+
+    public void loadPreferences (File prefFile) {
         DocumentBuilder dBuilder = null;
         try {
             dBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
@@ -34,7 +41,7 @@ public class Parser {
         catch (SAXException e) {
             e.printStackTrace();
         }
-        
+
         if (doc.hasChildNodes()) {
             parse(doc.getChildNodes());
         }
@@ -42,10 +49,57 @@ public class Parser {
     }
 
     private void parse (NodeList childNodes) {
-        for (int count=0; count < childNodes.getLength(); count++){
-            
+
+        for (int count = 0; count < childNodes.getLength(); count++) {
+            Node tempNode = childNodes.item(count);
+
+            if (tempNode.getNodeType() == Node.ELEMENT_NODE) {
+                String nodeName = tempNode.getNodeName();
+
+                if (tempNode.hasAttributes()) {
+                    NamedNodeMap nodeMap = tempNode.getAttributes();
+                    Node node = nodeMap.item(0);
+                    
+                    if ("background".equals(nodeName)) {       
+                        System.out.println(nodeName + " " +node.getNodeValue());
+                    }
+                    
+                    if ("turtles".equals(nodeName)){
+                        System.out.println(nodeName + " " +node.getNodeValue());
+                    }
+                    
+                    if ("file".equals(nodeName)){
+                        System.out.println(nodeName + " " +node.getNodeValue());
+                    }
+                    
+                    if ("turtleImage".equals(nodeName)){
+                        System.out.println(nodeName + " " +node.getNodeValue());
+                    }
+                    
+                    if ("grid".equals(nodeName)){
+                        System.out.println(nodeName + " " +node.getNodeValue());
+                    }
+                    
+                    if ("status".equals(nodeName)){
+                        System.out.println(nodeName + " " +node.getNodeValue());
+                    }
+                    
+
+                }
+
+            }
+
+            if (tempNode.hasChildNodes()) {
+                parse(tempNode.getChildNodes());
+            }
         }
-        
+
+    }
+
+    public static void main (String[] args) {
+        Parser p = new Parser();
+        File f = new File("pref.xml");
+        p.loadPreferences(f);
     }
 
 }
