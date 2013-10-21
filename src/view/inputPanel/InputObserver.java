906e1d867fe0c5d8796405e5ebb576caaeb06a64
Lalita Maraj <lalita.maraj@duke.edu>
2013-10-21 13:38:54 -0400

    10/21_added javadoc to input observer class

--- src/view/inputPanel/InputObserver.java
+++ src/view/inputPanel/InputObserver.java
@@ -2,6 +2,10 @@ package view.inputPanel;
 
 public interface InputObserver {
 
+    /**Passes booleans to notify whether actions can be performed
+     * @param canUndo indicates whether undo is currently doable
+     * @param canRedo indicates whether redo is currently doable
+     */
     void update (Boolean canUndo, Boolean canRedo);
 
 }
